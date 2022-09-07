66762ac7abf86b6ee0ac78bcf0aadbe8f2342803
Aaron Sears <aeroaaron200@gmail.com>
2022-09-07 15:11:23 -0600

    Initial Commit

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Aaron Sears on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
